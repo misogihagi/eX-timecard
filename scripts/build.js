e57387a70f8c27b4e236e720c9d1d4d9b80d936c
misogihagi <hagimiso@gmail.com>
2022-06-18 02:27:31 +0900

    modify build script to deploy

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,20 @@
+const fsp = require('fs/promises')
 const { build } = require('esbuild');
 
+const sdir = __dirname +'/../src/public/';
+const ddir = __dirname +'/../dist/';
+
+(async ()=>{
+  try{
+    await fsp.mkdir(ddir)
+  }catch{
+  }finally{
+    await Promise.all(
+      (await fsp.readdir(sdir))
+        .map(f=>fsp.copyFile(sdir+f,ddir+f))
+    )
+  }
+})()
 
 build({
   entryPoints: ["src/main.js"],
